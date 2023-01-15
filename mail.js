9597b0be5cd5d4c9cb3fbee95358996c70132bdf
luca.dragonetti <luca.dragonetti@blacksheep.srl>
2023-01-16 00:57:07 +0100

    update

--- mail.js
+++ mail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail')
+
 
 const nodemail = {
-    transporter: nodemailer.createTransport({
+    /*transporter: nodemailer.createTransport({
         host: "smtp-relay.sendinblue.com",
         port: 587,
         secure: false, // true for 465, false for other ports
@@ -9,20 +11,37 @@ const nodemail = {
             user: "lucadrago96@hotmail.com", // generated ethereal user
             pass: "6mw9h72Lz1p3dbFJ", // generated ethereal password
         },
-    }),
+    }),*/
 
 
     sendMail: async function (from, to, subject, text, html, attachments)  {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
         // send mail with defined transport object
         console.log("invio mail")
-        let info = await this.transporter.sendMail({
-            from: from, // sender address
-            to: to, // list of receivers
-            subject: subject, // Subject line
+        const msg = {
+            to: to, // Change to your recipient
+            from: from, // Change to your verified sender
+            subject: subject,
             text: text,
             html: html,
-            attachments: attachments // plain text body
-        });
+            attachments:attachments
+          }
+          sgMail
+            .send(msg)
+            .then(() => {
+              console.log('Email sent')
+            })
+            .catch((error) => {
+              console.error(error)
+            })
+    //    < let info = await this.transporter.sendMail({
+    //         from: from, // sender address
+    //         to: to, // list of receivers
+    //         subject: subject, // Subject line
+    //         text: text,
+    //         html: html,
+    //         attachments: attachments // plain text body
+    //     });>
     
         console.log("Message sent: %s", info.messageId);
 
