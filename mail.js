f9eace9eb148434f8c4c62225f8ad3a56c47054f
luca.dragonetti <luca.dragonetti@blacksheep.srl>
2023-01-16 01:27:31 +0100

    revert to sendinblue

--- mail.js
+++ mail.js
@@ -1,9 +1,8 @@
 const nodemailer = require("nodemailer");
-const sgMail = require('@sendgrid/mail')
 
 
 const nodemail = {
-    /*transporter: nodemailer.createTransport({
+    transporter: nodemailer.createTransport({
         host: "smtp-relay.sendinblue.com",
         port: 587,
         secure: false, // true for 465, false for other ports
@@ -11,41 +10,22 @@ const nodemail = {
             user: "lucadrago96@hotmail.com", // generated ethereal user
             pass: "6mw9h72Lz1p3dbFJ", // generated ethereal password
         },
-    }),*/
+    }),
 
 
     sendMail: async function (from, to, subject, text, html, attachments)  {
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        console.log(process.env.SENDGRID_API_KEY)
         // send mail with defined transport object
         console.log("invio mail")
-        const msg = {
-            to: to, // Change to your recipient
-            from: from, // Change to your verified sender
-            subject: subject,
+        let info = await this.transporter.sendMail({
+            from: from, // sender address
+            to: to, // list of receivers
+            subject: subject, // Subject line
             text: text,
             html: html,
-            attachments:attachments
-          }
-
-          console.log(to)
-          sgMail
-            .send(msg)
-            .then(() => {
-              console.log('Email sent')
-            })
-            .catch((error) => {
-              console.error(error)
-            })
-    //    < let info = await this.transporter.sendMail({
-    //         from: from, // sender address
-    //         to: to, // list of receivers
-    //         subject: subject, // Subject line
-    //         text: text,
-    //         html: html,
-    //         attachments: attachments // plain text body
-    //     });>
+            attachments: attachments // plain text body
+        });
     
+        console.log("Message sent: %s", info.messageId);
 
     }
 }
